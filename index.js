e7f265d146214ea2c5dcb457e4e0abcedda15cdc
zhujun-max <2368422640@qq.com>
2022-07-01 19:09:58 +0800

    修改

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var fs = require('fs')
-console.log('1111')
+// console.log('1111')
 fs.readdir(process.cwd(), function (err, data) {
   var stats = [];    //避免再次执行fs.stat
-  console.log('222222222')
+  // console.log('222222222')
   // console.log("");
   // if (!data.length) {
   //   return console.log('\033[31m No data to show!\033[39m\n');
@@ -10,7 +10,7 @@ fs.readdir(process.cwd(), function (err, data) {
   // console.log('	选择你要查看的文件或目录！\n');
   // console.log(data);
   data.map((d) => {
-    console.log(d)
+    // console.log(d)
     // console.log(d);
     // 使用stat()方法，区分是文件还是目录
     //语法：fs.stat(路径+/+遍历后的数据d,回调函数)
@@ -18,7 +18,7 @@ fs.readdir(process.cwd(), function (err, data) {
       if (err) throw err
       if (stat.isDirectory()) {
         //判断当前文件是否是目录
-        // console.log('目录:' + d);
+        console.log(d);
       }else if (stat.isFile()) {
         //判断当前文件是否是普通文件
         // console.log('文件:'+d);
